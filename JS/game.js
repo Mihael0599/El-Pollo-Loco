d4533364a7b6b0762726384a5ec6aa0d0fc9b3a1
Mihael0599 <mihaeldraz10@gmail.com>
2025-02-05 20:29:51 +0100

    added js

---
+++ JS/game.js
@@ -0,0 +1,15 @@
+let canvas;
+let ctx;
+let character = new Character();
+let enemies = [
+    new Chicken(),
+    new Chicken(),
+    new Chicken()
+]
+
+let new World();
+
+
+function init() {
+    canvas = document.getElementById("canvas");
+}
